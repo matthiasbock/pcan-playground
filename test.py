05728263f78a8e0cf99b763e439bcb3c9b864fc8
Matthias Bock <mail@matthiasbock.net>
2017-11-21 17:18:00 +0100

    Added code to scan for USB adapters

--- test.py
+++ test.py
@@ -1,30 +1,48 @@
+#!/usr/bin/python
 
 from time import sleep
 from ctypes import *
 from string import *
-from PCANBasic import *
 
+from PCAN_Basic.Include.PCANBasic import *
 
-print("Initializing adapter...")
+m_CHANNELS = {'PCAN_USBBUS1':PCAN_USBBUS1,
+                           'PCAN_USBBUS2':PCAN_USBBUS2, 'PCAN_USBBUS3':PCAN_USBBUS3, 'PCAN_USBBUS4':PCAN_USBBUS4, 'PCAN_USBBUS5':PCAN_USBBUS5,
+                           'PCAN_USBBUS6':PCAN_USBBUS6, 'PCAN_USBBUS7':PCAN_USBBUS7, 'PCAN_USBBUS8':PCAN_USBBUS8, 'PCAN_USBBUS9':PCAN_USBBUS9,
+                           'PCAN_USBBUS10':PCAN_USBBUS10, 'PCAN_USBBUS11':PCAN_USBBUS11, 'PCAN_USBBUS12':PCAN_USBBUS12, 'PCAN_USBBUS13':PCAN_USBBUS13,
+                           'PCAN_USBBUS14':PCAN_USBBUS14, 'PCAN_USBBUS15':PCAN_USBBUS15, 'PCAN_USBBUS16':PCAN_USBBUS16 }
 
 m_objPCANBasic = PCANBasic()
+
+# Find available USB adapters
+items = []
+for name, value in m_CHANNELS.iteritems():
+    # Checks for a Plug&Play Handle and, according with the return value, includes it
+    # into the list of available hardware channels.
+    #
+    result = m_objPCANBasic.GetValue(value, PCAN_CHANNEL_CONDITION)
+    if  (result[0] == PCAN_ERROR_OK) and (result[1] & PCAN_CHANNEL_AVAILABLE):
+        print("Channel available: ", name)
+        result = m_objPCANBasic.GetValue(value, PCAN_CHANNEL_FEATURES)
+        items.append(name)
+
 m_PcanHandle = PCAN_USBBUS1
 baudrate = PCAN_BAUD_1M
-#hwtype = PCAN_TYPE_ISA_SJA
-hwtype = PCAN_TYPE_ISA
+hwtype = PCAN_TYPE_ISA_SJA
+# hwtype = PCAN_TYPE_ISA
 ioport = 0x02a0
 interrupt = 11
 
-#result =  m_objPCANBasic.Initialize(m_PcanHandle,baudrate,hwtype,ioport,interrupt)
+result = m_objPCANBasic.Initialize(m_PcanHandle, baudrate, hwtype, ioport, interrupt)
 
 
-## Processes a received message, in order to show it in the Message-ListView
-##
-def ProcessMessage(*args):        
+# # Processes a received message, in order to show it in the Message-ListView
+# #
+def ProcessMessage(*args):
     # Split the arguments. [0] TPCANMsg, [1] TPCANTimestamp
     #
     theMsg = args[0][0]
-    itsTimeStamp = args[0][1]    
+    itsTimeStamp = args[0][1]
 
     newMsg = TPCANMsg()
     newMsg.ID = theMsg.ID
