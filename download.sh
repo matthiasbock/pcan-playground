dda8d4105a730b71d69e8acb187bf8dbce335888
Matthias Bock <mail@matthiasbock.net>
2017-11-21 17:46:48 +0100

    Switched to using curl instead of wget

--- download.sh
+++ download.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget -c http://www.peak-system.com/produktcd/Develop/PC%20interfaces/Linux/PCAN-Basic_API_for_Linux/PCAN_Basic_Linux-4.2.0.tar.gz
+curl http://www.peak-system.com/produktcd/Develop/PC%20interfaces/Linux/PCAN-Basic_API_for_Linux/PCAN_Basic_Linux-4.2.0.tar.gz
 tar -xzf PCAN_Basic_Linux-4.2.0.tar.gz
 
 # Applying patches
@@ -12,7 +12,7 @@ cd PCAN_Basic_Linux-4.2.0/pcanbasic
 make && ln -s libpcanbasic.so ../..
 cd ../..
 
-wget -c https://www.peak-system.com/fileadmin/media/files/pcan-basic.zip
+curl https://www.peak-system.com/fileadmin/media/files/pcan-basic.zip
 unzip -o pcan-basic.zip -d PCAN_Basic
 
 # Necessary in order to enable import
