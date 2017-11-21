77fbddb752836b5c8b7b484a1ab82c661424bf72
Matthias Bock <mail@matthiasbock.net>
2017-11-21 13:54:02 +0100

    Added script to download Peak libraries

---
+++ download.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget -c http://www.peak-system.com/produktcd/Develop/PC%20interfaces/Linux/PCAN-Basic_API_for_Linux/PCAN_Basic_Linux-4.2.0.tar.gz
+tar -xzf PCAN_Basic_Linux-4.2.0.tar.gz
+
+wget -c https://www.peak-system.com/fileadmin/media/files/pcan-basic.zip
+unzip -o pcan-basic.zip -d PCAN_Basic
+
