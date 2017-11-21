510ad25e40d0a1539c2226068c35f6ce88bca867
Matthias Bock <mail@matthiasbock.net>
2017-11-21 14:33:11 +0100

    Added patch to fix issue #1

--- download.sh
+++ download.sh
@@ -3,6 +3,14 @@
 wget -c http://www.peak-system.com/produktcd/Develop/PC%20interfaces/Linux/PCAN-Basic_API_for_Linux/PCAN_Basic_Linux-4.2.0.tar.gz
 tar -xzf PCAN_Basic_Linux-4.2.0.tar.gz
 
+# Applying patch to Makefile
+patch -p0 < p1.patch
+
+# Compile .so
+cd PCAN_Basic_Linux-4.2.0/pcanbasic
+make
+cd ../..
+
 wget -c https://www.peak-system.com/fileadmin/media/files/pcan-basic.zip
 unzip -o pcan-basic.zip -d PCAN_Basic
 
